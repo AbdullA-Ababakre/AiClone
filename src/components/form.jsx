33b8e795789b739e8c01253be5ec3bab529d702a
Abdulla-Ababakre <abdullaszu@gmail.com>
2023-10-31 01:19:11 -0700

    feat: submitted form

---
+++ src/components/form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FormComponent() {
+  return <div><iframe id="gform" src="https://forms.gle/p2foynQCaSPEWNRP9" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></div>;
+}
+
+export default FormComponent;
